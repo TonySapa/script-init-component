63fe2fcbd13c126a1844a8d0a35ba52bcd180203
TonySapa <antoniosapa90@gmail.com>
2022-05-27 17:01:02 +0200

    Updated stories generated file to match eslint configuration

--- contents/stories.js
+++ contents/stories.js
@@ -1,30 +1,30 @@
-const stories = (componentName) => `import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+const stories = (componentName) => `import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import ${componentName} from '.';
+import ${componentName} from '.${componentName}'
 
 export default {
   title: 'Components/${componentName}',
   component: ${componentName},
-} as ComponentMeta<typeof ${componentName}>;
+} as ComponentMeta<typeof ${componentName}>
 
-const Template: ComponentStory<typeof ${componentName}> = (args: any) => <${componentName} {...args} />;
+const Template: ComponentStory<typeof ${componentName}> = (args: any) => <${componentName} {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 /* Primary.parameters = {
   backgrounds: { default: 'dark' }
-};
+}
 
 Primary.args = {
   label: '${componentName}',
   variant: ${componentName}Variants.primary
-};
+}
 
-export const Secondary = Template.bind({});
+export const Secondary = Template.bind({})
 Secondary.args = {
   label: '${componentName}',
   variant: ${componentName}Variants.secondary
-}; */
+} */
 `
 
 export default stories
